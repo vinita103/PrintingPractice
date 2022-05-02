48bb8f22edc85f32cb4be6108b2fb67d6b542ef2
vinita103 <101148175+vinita103@users.noreply.github.com>
2022-05-02 13:17:06 -0400

    adding java practice files

---
+++ src/HiFriend.java
@@ -0,0 +1,11 @@
+/**
+ * HiFriend
+ */
+public class HiFriend {
+    public static void main(String[] args) {
+        String F = "Hi " + "Larry " + "Sprinkle" + "!";
+        System.out.println(F);
+    }
+
+    
+}
---
+++ src/MyNameIs.java
@@ -0,0 +1,7 @@
+public class MyNameIs {
+    public static void main(String[] args) throws Exception {
+        System.out.println("My name is");
+        System.out.println("Larry");
+        System.out.println("Sprinkle");
+    }
+}
